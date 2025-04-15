e42da450970c968297964cdb93d9efd6faa83210
jake22789 <70212338+jake22789@users.noreply.github.com>
2025-04-15 15:24:13 -0600

    commit changes

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b018242c42525ee61b65dbbdd300c9544c4b66ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86d7ea7a49f8258f93a0c1f87b29ac956abb7f1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
